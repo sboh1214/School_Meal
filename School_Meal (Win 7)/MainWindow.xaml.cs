ea0e6e7054a845651a9a19f60e45b0701f775cc6
승빈 오 <sboh1214@gmail.com>
2018-07-15 14:22:40 +0900

    (Add) WeekWindow

--- School_Meal (Win 7)/MainWindow.xaml.cs
+++ School_Meal (Win 7)/MainWindow.xaml.cs
@@ -15,7 +15,7 @@ namespace School_Meal__Win_7_
             var Date = DateTime.Now;
             TodayDate_TextBlock.Text = Date.Year.ToString() + "년 " + Date.Month.ToString() + "월 " + Date.Day.ToString() + "일";
 
-            Main_Frame.Navigate(content: TodayWindow);
+            //Main_Frame.Navigate(content: TodayWindow);
         }
     }
 }
